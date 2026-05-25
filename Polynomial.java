d927c0040cc7a2d5ca6e9204201d7f1dd09a24a8
Satyajit Datta <dattasa2@utsc-labs.utoronto.ca>
2026-05-25 14:59:44 -0400

    implementd add

--- Polynomial.java
+++ Polynomial.java
@@ -12,8 +12,30 @@ public class Polynomial {
     public Polynomial add(Polynomial p) {
         int calling_length = this.coefficients.length;
         int arg_length = this.coefficients.length;
-
-        return p;
+        longer = calling_length;
+	if (arg_length > calling_length) {
+		longer = arg_length
+	}
+	
+	double[] coeff = new double[longer];
+	for (int i = 0; i < longer; i++) {
+		double a = 0.0;
+		try {
+			a = calling_length[i] 
+		} catch {
+			a = 0.0
+		}
+		double b = 0.0;
+		try {
+			b = arg_length[i]
+		}
+		catch {
+			b = 0.0
+		}
+		coeff[i] = a + b;
+	}
+	Polynomial result = new Polynomial(coeff);
+        return result;
     }
 
     public double evaluate(double num) {
